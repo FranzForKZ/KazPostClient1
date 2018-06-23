1e418fa72bf0cbe3fdd9962661ae6a6029706e86
FranzForKZ <40528563+FranzForKZ@users.noreply.github.com>
2018-06-24 02:39:08 +0300

    загрузка11
    загрузка11

---
+++ RebootDialog/RebootDialog.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace RebootDialog
+{
+    partial class RebootDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RebootDialog));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 99);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(698, 257);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // checkBox
+            // 
+            this.checkBox.AutoSize = true;
+            this.checkBox.Checked = true;
+            this.checkBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox.Location = new System.Drawing.Point(281, 134);
+            this.checkBox.Name = "checkBox";
+            this.checkBox.Size = new System.Drawing.Size(141, 17);
+            this.checkBox.TabIndex = 1;
+            this.checkBox.Text = "Перезагрузить сейчас";
+            this.checkBox.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(27, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(648, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Для корректной установки \"АРМ оператора\" необходимо перезагрузить компьютер";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(296, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Установка почти завершена";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(293, 362);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Подтвердить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // RebootDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 447);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "RebootDialog";
+            this.Text = "KazPostArm";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.RebootDialog_FormClosing);
+            this.Load += new System.EventHandler(this.RebootDialog_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RebootDialog/RebootDialog.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+namespace RebootDialog
+{
+    public partial class RebootDialog : Form
+    {
+        //with api
+        //[StructLayout(LayoutKind.Sequential, Pack = 1)]
+        //internal struct TokPriv1Luid { public int Count; public long Luid; public int Attr;}
+        //[DllImport("kernel32.dll", ExactSpelling = true)]
+        //internal static extern IntPtr GetCurrentProcess();
+        //[DllImport("advapi32.dll", ExactSpelling = true, SetLastError = true)]
+        //internal static extern bool OpenProcessToken(IntPtr h, int acc, ref IntPtr phtok);
+        //[DllImport("advapi32.dll", SetLastError = true)]
+        //internal static extern bool LookupPrivilegeValue(string host, string name, ref long pluid);
+        //[DllImport("advapi32.dll", ExactSpelling = true, SetLastError = true)]
+        //internal static extern bool AdjustTokenPrivileges(IntPtr htok, bool disall, ref TokPriv1Luid newst, int len, IntPtr prev, IntPtr relen);
+        //[DllImport("user32.dll", ExactSpelling = true, SetLastError = true)]
+        //internal static extern bool ExitWindowsEx(int flg, int rea);
+        //internal const int SE_PRIVILEGE_ENABLED = 0x00000002;
+        //internal const int TOKEN_QUERY = 0x00000008;
+        //internal const int TOKEN_ADJUST_PRIVILEGES = 0x00000020;
+        //internal const string SE_SHUTDOWN_NAME = "SeShutdownPrivilege";
+        //internal const int EWX_LOGOFF = 0x00000000;
+        //internal const int EWX_SHUTDOWN = 0x00000001;
+        //internal const int EWX_REBOOT = 0x00000002;
+        //internal const int EWX_FORCE = 0x00000004;
+        //internal const int EWX_POWEROFF = 0x00000008;
+        //internal const int EWX_FORCEIFHUNG = 0x00000010;
+        //private void DoExitWin(int flg)
+        //{
+        //    bool ok;
+        //    TokPriv1Luid tp;
+        //    IntPtr hproc = GetCurrentProcess();
+        //    IntPtr htok = IntPtr.Zero;
+        //    ok = OpenProcessToken(hproc, TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, ref htok);
+        //    tp.Count = 1;
+        //    tp.Luid = 0;
+        //    tp.Attr = SE_PRIVILEGE_ENABLED;
+        //    ok = LookupPrivilegeValue(null, SE_SHUTDOWN_NAME, ref tp.Luid);
+        //    ok = AdjustTokenPrivileges(htok, false, ref tp, 0, IntPtr.Zero, IntPtr.Zero);
+        //    ok = ExitWindowsEx(flg, 0);
+        //}
+        //public void RestartComputerApi()
+        //{
+        //    Thread.Sleep(4000);
+        //    DoExitWin(EWX_REBOOT);
+        //}
+        PostUserActivity.Net.NetRequests NetReq = new PostUserActivity.Net.NetRequests();
+        PostUserActivity.Contracts.Network.ResponseResult ResResult;
+        bool isDefind = false;
+        public RebootDialog()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+            this.TopLevel = true;
+            //ResResult = NetReq.RegisterArm(PostUserActivity.HW.SystemInfo.GetUUID(), PostUserActivity.HW.SystemInfo.GetCurrentUserDomainName());
+            //MessageBox.Show("код ответа сервера: " + (int)ResResult.RequestResult);
+           
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          //string gg = CommonLib.AppInfo.Version;
+
+            Application.Exit();
+
+        }
+
+        private void RebootDialog_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (checkBox.Checked)
+            {
+                try
+                {
+                    var psi = new ProcessStartInfo("shutdown", "/r /t 30");
+                    psi.CreateNoWindow = true;
+                    psi.UseShellExecute = false;
+                    Process.Start(psi);
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show("Произошла ошибка:" + ex.Message + "\n" + ex.StackTrace + "\n" + ex.Source + "\n" + ex.InnerException);
+                }
+                
+            }
+        }
+
+        private void RebootDialog_Load(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+            timer.Interval = 10000;
+            timer.Tick += (s, Z) =>
+            {
+                ((System.Windows.Forms.Timer)s).Stop(); //s is the Timer
+                checkBox.Checked = false;
+                Application.Exit();
+            };
+            timer.Start();
+            
+        }
+
+       
+    }
+}
